dba67978d0f7a9ffa7f412a0b471390c9c4e84da
kluhuks <progem95@mail.ru>
2025-01-31 16:11:12 +0300

    123

--- test1.py
+++ test1.py
@@ -10,36 +10,36 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 options = Options()
-options.add_argument("--no-sandbox")  # Отключение песочницы
-options.add_argument("--disable-dev-shm-usage")  # Исправление для Linux
-options.add_argument("--disable-gpu")  # Отключение GPU (иногда помогает)
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")  
 
-service = Service("C:\Program Files\Google\Chrome\Application\chromedriver-win64\chromedriver.exe")  # Укажи путь к ChromeDriver
+service = Service("C:\Program Files\Google\Chrome\Application\chromedriver-win64\chromedriver.exe")  
 driver = webdriver.Chrome(service=service, options=options)
 def scen_1():
     try:
         
-        start_time = time.time()  # Запуск таймера
+        start_time = time.time() 
 
-        # Открытие главной страницы Википедии
+        
         driver.get("https://www.wikipedia.org/")
         driver.maximize_window()
-        time.sleep(1)  # Даем время на загрузку страницы
+        time.sleep(1)  
 
-        # Поиск строки поиска
+
         search_box = driver.find_element(By.NAME, "search")
 
-        # Ввод ключевого слова и нажатие Enter
+ 
         search_box.send_keys("Солнечная система")
         search_box.send_keys(Keys.RETURN)
         
-        time.sleep(2)  # Ожидание загрузки результатов
+        time.sleep(2)
 
     finally:
-        end_time = time.time()  # Завершение таймера
+        end_time = time.time() 
         execution_time = end_time - start_time
         
-        # Закрытие браузера
+
         driver.quit()
         return execution_time
 
